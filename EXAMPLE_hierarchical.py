62a1ae3fa8ba0ddfd20113fe049feb735f97188a
tollsv <32487670+tollsv@users.noreply.github.com>
2018-03-20 13:09:14 -0400

    Update EXAMPLE_hierarchical.py

--- EXAMPLE_hierarchical.py
+++ EXAMPLE_hierarchical.py
@@ -13,5 +13,5 @@ import pandas as pd
 import numpy as np
 
 df2 = pd.DataFrame(np.random.randint(low=0, high=10, size=(5, 5)), columns=['a', 'b', 'c', 'd', 'e'], index=["s1","s2","s3","s4","s5"])
-hmap,source,clusters = heatmap(df2,3,title="Sample Heatmap",plot_height=200)
+hmap,clusters = heatmap(df2,3,title="Sample Heatmap",plot_height=200)
 show(hmap)
